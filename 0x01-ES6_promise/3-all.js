5e80ebebf1378718d310a3e90d5659a3163641a4
Charawey-X <charawey@gmail.com>
2022-12-21 09:32:55 +0300

    handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,7 @@
-import { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((values) => {
-    console.log(values[0].body);
-    console.log(values[1].firstName);
-    console.log(values[1].lastName);
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
   });
 }
