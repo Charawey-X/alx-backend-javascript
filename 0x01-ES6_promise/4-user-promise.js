3f478052a29ba5571a949bf720a4103012e3f409
Charawey-X <charawey@gmail.com>
2022-12-21 10:48:48 +0300

    return resolved promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,3 @@
 export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({ firstName, lastName }).then(
-    (value) => { console.log(value); },
-  ).catch(() => Error());
+  return Promise.resolve({ firstName, lastName });
 }
