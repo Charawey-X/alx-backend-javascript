98abbf428c4a870af4bab0fee012826dd246c0e0
Charawey-X <charawey@gmail.com>
2022-12-21 08:46:18 +0300

    append handlers to promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -15,9 +15,4 @@ export default function handleResponseFromAPI(promise) {
       console.log('Got a response from the API');
     },
   );
-  fullPromise.catch(
-    () => {
-      console.log('Got a response from the API');
-    },
-  );
 }
