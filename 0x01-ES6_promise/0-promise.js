0b0027b6d7bebadd6d00ce43d99524e9a939e3df
Charawey-X <charawey@gmail.com>
2022-12-21 07:38:41 +0300

    creating a promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+export default function getResponseFromAPI() {
+  const promise = new Promise((resolve, reject) => {
+    const example1 = true;
+    if (example1) {
+      resolve("First Promise");
+    }
+    else {
+      reject("Couldn't keep a simple promise");
+    }
+  });
+  return promise;
+}
