469ef5abb68123ff6c909adc449b7738adbf7f12
Charawey-X <charawey@gmail.com>
2022-12-23 22:45:19 +0300

    return a new object of the class

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,15 +1,13 @@
-import Currency from './3-currency';
-
 export default class Car {
   /* eslint no-underscore-dangle: 0 */
 
   constructor(brand, motor, color) {
-    this._brand = typeof brand === 'string' ? brand : Currency._throw('brand must be a string!');
-    this._motor = typeof motor === 'string' ? motor : Currency._throw('motor must be a string!');
-    this._color = typeof color === 'string' ? color : Currency._throw('color must be a string!');
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color);
+    return new this.constructor();
   }
 }
