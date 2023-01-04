63b7ada3b17cca567b0f7a309deecefa9fe459eb
Charawey-X <charawey@gmail.com>
2023-01-04 14:52:32 +0300

    appending set values

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,3 @@
-export default function cleanSet(set, startString = '') {
-  return [...set].filter((string) => startString.length !== 0 && string.startsWith(startString)).map((string) => string.replace(startString, '')).join('-') || '';
+export default function cleanSet(set, startString) {
+  return typeof startString === 'string' ? [...set].filter((string) => startString.length !== 0 && string.startsWith(startString)).map((string) => string.replace(startString, '')).join('-') : '';
 }
--- 0x03-ES6_data_manipulation/tests/8-main.js
+++ 0x03-ES6_data_manipulation/tests/8-main.js
@@ -2,4 +2,4 @@ import cleanSet from "../8-clean_set.js";
 
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 1));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', '1bonappetit', 'banana']), 1));
