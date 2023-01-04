57c4d906e09dc3208441fdf09b32f3c3484c75dd
Charawey-X <charawey@gmail.com>
2023-01-04 14:43:53 +0300

    appending set values

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,3 @@
 export default function cleanSet(set, startString = '') {
-  return [...set].filter((string) => startString.length !== 0 && typeof startString == 'string' && string.startsWith(startString)).map((string) => string.replace(startString, '')).join('-');
+  return [...set].filter((string) => startString.length !== 0 && string.startsWith(startString)).map((string) => string.replace(startString, '')).join('-') || '';
 }
